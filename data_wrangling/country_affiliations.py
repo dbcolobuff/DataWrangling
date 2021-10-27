1fcb96d19ebb56bb88c499c6aacac4a7b28e55a0
Daniel Bonnecaze <danielbonnecaze@gmail.com>
2021-10-27 00:21:55 -0600

    usa and uk formatting done

--- data_wrangling/country_affiliations.py
+++ data_wrangling/country_affiliations.py
@@ -7,7 +7,6 @@ with open(r'C:\\projects\\data_wrangling\\affiliations.csv', 'r', errors='ignore
         row = row[0].split(',')
         # print(row[len(row)-1])
         countries = []
-        #if row[0]
         countries.append(row[len(row)-1])
         #print(type(countries[0]))
         #print(countries[0])
@@ -22,5 +21,36 @@ with open(r'C:\\projects\\data_wrangling\\affiliations.csv', 'r', errors='ignore
             countries.remove(countries[0])
         elif len(countries) == 2:
             countries.remove(countries[0])
-        print(countries)
-    file.close()
+        #print(countries[0][len(countries[0])-1])
+        with open(r'C:\\projects\\data_wrangling\\usa_states.csv', 'r', errors='ignore') as f:
+            reader2 = csv.reader(f)
+            next(reader2)
+            data = list(reader2)
+            #print(data[1][0])
+            #print(countries[0])
+            i = 0
+            while i < len(data):
+                if countries[0] == data[i][0]:
+                    countries = [countries[0].replace(countries[0], 'United States') for countries[0] in countries]
+                elif countries[0] == data[i][1]:
+                    countries = [countries[0].replace(countries[0], 'United States') for countries[0] in countries]
+                elif countries[0][len(countries[0])-1].isnumeric() == True:
+                    countries = [countries[0].replace(countries[0], 'Canada') for countries[0] in countries]
+                elif len(countries) >= 2 and countries[len(countries)-1][0] == 'U':
+                    countries.remove(countries[0])
+                if countries[0][0] == 'U' and countries[0][1] == '.':
+                    if countries[0][2] == 'S':
+                        countries = [countries[0].replace(countries[0], 'United States') for countries[0] in countries]
+                    elif countries[0][2] == 'K':
+                        countries = [countries[0].replace(countries[0], 'United Kingdom') for countries[0] in countries]
+                elif countries[0][1] == 'S':
+                    countries = [countries[0].replace(countries[0], 'United States') for countries[0] in countries]
+                elif countries[0][1] == 'K':
+                    countries = [countries[0].replace(countries[0], 'United Kingdom') for countries[0] in countries]
+                i += 1
+            print(countries)
+        f.close() 
+file.close()
+
+
+
