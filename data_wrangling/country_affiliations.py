3799710f523abc5751e24547721ebe591e1fd525
Daniel Bonnecaze <danielbonnecaze@gmail.com>
2021-10-26 18:32:38 -0600

    Post code delter very close

--- data_wrangling/country_affiliations.py
+++ data_wrangling/country_affiliations.py
@@ -4,8 +4,23 @@ with open(r'C:\\projects\\data_wrangling\\affiliations.csv', 'r', errors='ignore
     reader = csv.reader(file, delimiter = ",")
     next(reader)
     for row in reader:
-        country = row[0].split(',')
-        print(country[len(country)-1])
-file.close
-
-
+        row = row[0].split(',')
+        # print(row[len(row)-1])
+        countries = []
+        #if row[0]
+        countries.append(row[len(row)-1])
+        #print(type(countries[0]))
+        #print(countries[0])
+        countries = countries[0].split()
+        #print(countries[0])
+        if countries[0].isnumeric() == True or '-' in countries[0]:
+            countries.remove(countries[0])
+        elif len(countries) > 1 and countries[len(countries)-2].isnumeric() == True:
+            countries.remove(countries[len(countries)-2])
+        elif len(countries) > 2:
+            countries.remove(countries[0])
+            countries.remove(countries[0])
+        elif len(countries) == 2:
+            countries.remove(countries[0])
+        print(countries)
+    file.close()
